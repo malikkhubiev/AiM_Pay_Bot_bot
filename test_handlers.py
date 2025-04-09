4ea9fd24cc4a4d4d9aabcdf5ae03caa855adeb44
Malik Khubiev <malik.hubiev@mail.ru>
2025-04-09 22:17:26 +0300

    test

--- test_handlers.py
+++ test_handlers.py
@@ -116,7 +116,8 @@ async def finish_test(chat_id, telegram_id):
     await bot.send_message(chat_id, text)
     log.info(f"баллы набраны = {correct_count} из {test_questions} = {correct_count / test_questions}")
 
-    if correct_count / test_questions >= 0.8:
+    if (correct_count / test_questions) >= 0.8:
+        log.info("passed")
         url = SERVER_URL + "/update_passed_exam"
         user_data = {
             "telegram_id": telegram_id
