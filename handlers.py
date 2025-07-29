2ed92269b8f32e73e200242105d47ccbed7f418a
malikkhubiev <malik.hubiev@mail.ru>
2025-07-29 11:15:31 +0300

    1

--- handlers.py
+++ handlers.py
@@ -75,6 +75,8 @@ async def start(message: types.Message, telegram_id: str = None, username: str =
             keyboard.add(
                 InlineKeyboardButton("Начало работы 🏔️", callback_data='getting_started'),
                 # InlineKeyboardButton("Документы 📚", callback_data='documents'),
+                log.info(LANDING_URL)
+                log.info(f"{str(LANDING_URL)}")
                 InlineKeyboardButton("Сайт 📚", url=f"{str(LANDING_URL)}"),
             )
             log.info(f"send_message")
